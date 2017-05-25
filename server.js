80e148825e13f652b8953ecef3133bf86f667ab1
Ashish3Solanki3 <ashish3.solanki3@cognizant.com>
2017-05-25 07:36:48 +0000

    Session4

--- server.js
+++ server.js
@@ -5,7 +5,38 @@ var app = express();
 app.use(bodyParser.json());
 mongoose.connect('mongodb://localhost:27017/mittens');
 
+var Meow = mongoose.model('Meow', {
+ text: String
+  });
+
+app.use(express.static('public')); 
+
+
+
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!');
+});
+
+
+app.get('/meows', function(req,res,next){
+  Meow.find({}, function(err,meows){
+    return res.json(meows);
+  });
+});
+
+app.post('/meows', function(req, res, next){	
+		var newMeow = new Meow({
+			text: req.body.newMeow		
+		});
+		newMeow.save(function(err){
+			return res.send("Added Successfully");
+	});	
+});
+
+app.put('/meows/remove', function(req, res, next){	
+var meowId=req.body.meow._id;		
+		Meow.remove({_id: meowId}, function(err){
+			return res.send("Deleted Successfully");
+	});
 });
 
